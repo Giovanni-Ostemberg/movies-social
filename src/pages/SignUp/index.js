794d519107d6c8a5bf40bd81ba539f1d7eaa6fda
Giovanni-Ostemberg <gigio.ostemberg@gmail.com>
2020-10-15 21:50:16 -0400

    adicionando rotas de autenticacao

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        error: ""
+      };
+    
+      handleSignUp = e => {
+        e.preventDefault();
+        alert("Eu vou te registrar");
+      };
+    
+      render() {
+        return (
+          <div>
+            <form onSubmit={this.handleSignUp}>
+              {this.state.error && <p>{this.state.error}</p>}
+              <input
+                type="text"
+                placeholder="Nome de usuário"
+                onChange={e => this.setState({ username: e.target.value })}
+              />
+              <input
+                type="email"
+                placeholder="Endereço de e-mail"
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+              <input
+                type="password"
+                placeholder="Senha"
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+              <button type="submit">Cadastrar grátis</button>
+              <hr />
+              <a href="/">Fazer login</a>
+            </form>
+          </div>
+        );
+      }
+}
